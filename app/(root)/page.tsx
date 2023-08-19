d510d33832eeb6a4d67376866ebdea7dc68882e4
Ben Hur <bhur777@gmail.com>
2023-08-19 11:22:25 -0400

    small change to modals

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,16 @@
 "use client";
-import { Modal } from "@/components/ui/modal";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useEffect } from "react";
 
 export default function SetupPage() {
-	const onOpen = useStoreModal((state) => state.onOpen);
 	const isOpen = useStoreModal((state) => state.isOpen);
+	const onOpen = useStoreModal((state) => state.onOpen);
 	useEffect(() => {
 		if (!isOpen) {
 			onOpen();
 		}
-	}, [isOpen, onOpen]);
+	}, [onOpen, isOpen]);
 
 	return <div className="p-4">Root Page</div>;
 }
