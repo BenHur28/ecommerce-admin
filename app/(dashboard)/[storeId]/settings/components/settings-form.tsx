6223b20bbfd9c6267a84aa4c120e312993718707
Ben Hur <bhur777@gmail.com>
2023-08-21 20:24:26 -0400

    Create API route for settings form

--- app/(dashboard)/[storeId]/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/settings/components/settings-form.tsx
@@ -45,7 +45,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 		<>
 			<div className="flex items-center justify-between">
 				<Heading title="Settings" description="Manage Store Preferences" />
-				<Button variant="destructive" size="icon" onClick={() => {}}>
+				<Button
+					disabled={loading}
+					variant="destructive"
+					size="icon"
+					onClick={() => setOpen(true)}
+				>
 					<Trash className="h-4 w-4" />
 				</Button>
 			</div>
--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -2,11 +2,13 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export const POST = async (req: Request) => {
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { storeId: string } }
+) {
 	try {
 		const { userId } = auth();
 		const body = await req.json();
-
 		const { name } = body;
 
 		if (!userId) {
@@ -17,15 +19,18 @@ export const POST = async (req: Request) => {
 			return new NextResponse("Name is required", { status: 400 });
 		}
 
-		const store = await prismadb.store.create({
+		const store = await prismadb.store.updateMany({
+			where: {
+				id: params.storeId,
+				userId: userId,
+			},
 			data: {
-				name,
-				userId,
+				name: name,
 			},
 		});
 		return NextResponse.json(store);
 	} catch (error) {
-		console.log("[STORE_POST]", error);
+		console.log("[STORE_PATCH]", error);
 		return new NextResponse("Internal Error", { status: 500 });
 	}
-};
+}
