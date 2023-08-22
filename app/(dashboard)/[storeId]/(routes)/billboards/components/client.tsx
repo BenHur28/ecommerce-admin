47676047607709dcf65459786fc4c1d3c71ad25b
Ben Hur <bhur777@gmail.com>
2023-08-22 00:29:08 -0400

    refactor

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 export const BillboardClient = () => {
 	return (
@@ -10,7 +13,12 @@ export const BillboardClient = () => {
 					title="Billboards (0)"
 					description="Manage billboards for your store"
 				/>
+				<Button>
+					<Plus className="mr-2 h-4 w-4" />
+					Add New
+				</Button>
 			</div>
+			<Separator />
 		</>
 	);
 };
