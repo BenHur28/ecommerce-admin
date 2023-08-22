2142c215a3a88bd3342369b12eab3f32a3b97be6
Ben Hur <bhur777@gmail.com>
2023-08-22 04:11:23 -0400

    Create billboards page/form/client

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -4,8 +4,12 @@ import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
 
 export const BillboardClient = () => {
+	const router = useRouter();
+	const params = useParams();
+
 	return (
 		<>
 			<div className="flex items-center justify-between">
@@ -13,7 +17,9 @@ export const BillboardClient = () => {
 					title="Billboards (0)"
 					description="Manage billboards for your store"
 				/>
-				<Button>
+				<Button
+					onClick={() => router.push(`/${params.storeId}/billboards/new}`)}
+				>
 					<Plus className="mr-2 h-4 w-4" />
 					Add New
 				</Button>
