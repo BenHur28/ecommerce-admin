7c0bc952c8e73c94c2985dc4d9ec3f32ac51bdd6
Ben Hur <bhur777@gmail.com>
2023-08-23 08:49:03 -0400

    Refactor/rename category-form.tsx

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as z from "zod";
-import { Billboard } from "@prisma/client";
+import { Category } from "@prisma/client";
 
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
@@ -23,41 +23,44 @@ import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { Heading } from "@/components/ui/heading";
-import ImageUpload from "@/components/ui/image-upload";
+import {
+	Select,
+	SelectContent,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 
-interface BillboardFormProps {
-	initialData: Billboard | null;
+interface CategoryFormProps {
+	initialData: Category | null;
 }
 
 const formSchema = z.object({
-	label: z.string().min(1),
-	imageUrl: z.string().min(1),
+	name: z.string().min(1),
+	billboardId: z.string().min(1),
 });
 
-type BillboardFormValues = z.infer<typeof formSchema>;
+type CategoryFormValues = z.infer<typeof formSchema>;
 
-export const BillboardForm: React.FC<BillboardFormProps> = ({
-	initialData,
-}) => {
+export const CategoryForm: React.FC<CategoryFormProps> = ({ initialData }) => {
 	const [open, setOpen] = useState(false);
 	const [loading, setLoading] = useState(false);
 	const params = useParams();
 	const router = useRouter();
 
-	const title = initialData ? "Edit Billboard" : "Create Billboard";
-	const description = initialData ? "Edit Billboard" : "Add a new Billboard";
-	const toastMessage = initialData ? "Billboard updated" : "Billboard created";
+	const title = initialData ? "Edit category" : "Create category";
+	const description = initialData ? "Edit category" : "Add a new category";
+	const toastMessage = initialData ? "Category updated" : "Category created";
 	const action = initialData ? "Save changes" : "Create";
 
-	const form = useForm<BillboardFormValues>({
+	const form = useForm<CategoryFormValues>({
 		resolver: zodResolver(formSchema),
 		defaultValues: initialData || {
-			label: "",
-			imageUrl: "",
+			name: "",
+			billboardId: "",
 		},
 	});
 
-	const onSubmit = async (data: BillboardFormValues) => {
+	const onSubmit = async (data: CategoryFormValues) => {
 		try {
 			setLoading(true);
 			if (initialData) {
@@ -122,38 +125,46 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
 					onSubmit={form.handleSubmit(onSubmit)}
 					className="space-y-8 w-full"
 				>
-					<FormField
-						control={form.control}
-						name="imageUrl"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Background image</FormLabel>
-								<FormControl>
-									<ImageUpload
-										disabled={loading}
-										value={field.value ? [field.value] : []}
-										onChange={(url) => field.onChange(url)}
-										onRemove={() => field.onChange("")}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
 					<div className="grid grid-cols-3 gap-8">
 						<FormField
 							control={form.control}
-							name="label"
+							name="name"
 							render={({ field }) => (
 								<FormItem>
-									<FormLabel>Label</FormLabel>
+									<FormLabel>Name</FormLabel>
 									<FormControl>
 										<Input
 											disabled={loading}
-											placeholder="Billboard label"
+											placeholder="Category name"
 											{...field}
 										/>
 									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="billboardId"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Billboard</FormLabel>
+									<Select
+										disabled={loading}
+										onValueChange={field.onChange}
+										value={field.value}
+										defaultValue={field.value}
+									>
+										<FormControl>
+											<SelectTrigger>
+												<SelectValue
+													defaultValue={field.value}
+													placeholder="Select a billboard"
+												></SelectValue>
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent></SelectContent>
+									</Select>
 									<FormMessage />
 								</FormItem>
 							)}
