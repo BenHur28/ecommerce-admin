84f2676186083c50b870761a5de07fe9ae4ce3a6
Ben Hur <bhur777@gmail.com>
2023-08-23 09:54:57 -0400

    refactor route.ts for sizes

--- app/api/[storeId]/sizes/route.ts
+++ app/api/[storeId]/sizes/route.ts
@@ -10,18 +10,18 @@ export const POST = async (
 		const { userId } = auth();
 		const body = await req.json();
 
-		const { label, imageUrl } = body;
+		const { name, value } = body;
 
 		if (!userId) {
 			return new NextResponse("Unauthorized", { status: 401 });
 		}
 
-		if (!label) {
-			return new NextResponse("Label is required", { status: 400 });
+		if (!name) {
+			return new NextResponse("Name is required", { status: 400 });
 		}
 
-		if (!imageUrl) {
-			return new NextResponse("ImageUrl is required", { status: 400 });
+		if (!value) {
+			return new NextResponse("Value is required", { status: 400 });
 		}
 
 		if (!params.storeId) {
@@ -39,16 +39,16 @@ export const POST = async (
 			return new NextResponse("Unauthorized user", { status: 403 });
 		}
 
-		const billboard = await prismadb.billboard.create({
+		const size = await prismadb.size.create({
 			data: {
-				label,
-				imageUrl,
+				name,
+				value,
 				storeId: params.storeId,
 			},
 		});
-		return NextResponse.json(billboard);
+		return NextResponse.json(size);
 	} catch (error) {
-		console.log("[BILLBOARD_POST]", error);
+		console.log("[SIZE_POST]", error);
 		return new NextResponse("Internal Error", { status: 500 });
 	}
 };
@@ -62,14 +62,14 @@ export const GET = async (
 			return new NextResponse("Store id is required", { status: 400 });
 		}
 
-		const billboards = await prismadb.billboard.findMany({
+		const sizes = await prismadb.size.findMany({
 			where: {
 				storeId: params.storeId,
 			},
 		});
-		return NextResponse.json(billboards);
+		return NextResponse.json(sizes);
 	} catch (error) {
-		console.log("[BILLBOARD_GET]", error);
+		console.log("[SIZES_GET]", error);
 		return new NextResponse("Internal Error", { status: 500 });
 	}
 };
