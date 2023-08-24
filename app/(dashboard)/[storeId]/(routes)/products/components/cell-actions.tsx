a65c97a3a4bb46abcfde092f990ff8eb792ac734
Ben Hur <bhur777@gmail.com>
2023-08-24 01:39:30 -0400

    Fix hydration error with searchKey

--- app/(dashboard)/[storeId]/(routes)/products/components/cell-actions.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-actions.tsx
@@ -8,7 +8,7 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { BillboardColumn } from "./columns";
+import { ProductColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import toast from "react-hot-toast";
@@ -18,7 +18,7 @@ import axios from "axios";
 import { AlertModal } from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-	data: BillboardColumn;
+	data: ProductColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -36,11 +36,11 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 	const onDelete = async () => {
 		try {
 			setLoading(true);
-			await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+			await axios.delete(`/api/${params.storeId}/products/${data.id}`);
 			router.refresh();
-			toast.success("Billboard deleted");
+			toast.success("Product deleted");
 		} catch (error) {
-			toast.error("Make sure you remove all categories using this billboard");
+			toast.error("Something went wrong");
 		} finally {
 			setLoading(false);
 			setOpen(false);
@@ -67,7 +67,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 					<DropdownMenuSeparator />
 					<DropdownMenuItem
 						onClick={() =>
-							router.push(`/${params.storeId}/billboards/${data.id}`)
+							router.push(`/${params.storeId}/products/${data.id}`)
 						}
 					>
 						<Edit className="mr-4 h-4 w-4" />
