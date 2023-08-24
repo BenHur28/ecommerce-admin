0d60b1aa65072ade80bd20605fd6268c87bbee7b
Ben Hur <bhur777@gmail.com>
2023-08-24 01:03:03 -0400

    Update product form

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -1,5 +1,5 @@
 import prismadb from "@/lib/prismadb";
-import { BillboardClient } from "./components/client";
+import { ProductClient } from "./components/client";
 import { ProductColumn } from "./components/columns";
 import { format } from "date-fns";
 import { formatter } from "@/lib/utils";
@@ -25,7 +25,7 @@ const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
 		isFeatured: item.isFeatured,
 		isArchived: item.isArchived,
 		price: formatter.format(item.price.toNumber()),
-		category: item.category,
+		category: item.category.name,
 		size: item.size.name,
 		color: item.color.value,
 		createdAt: format(item.createdAt, "MMMM do, yyyy"),
@@ -34,7 +34,7 @@ const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
 	return (
 		<div className="flex-col">
 			<div className="flex- 1 space-y-4 p-8 pt-6">
-				<BillboardClient data={formattedProducts} />
+				<ProductClient data={formattedProducts} />
 			</div>
 		</div>
 	);
