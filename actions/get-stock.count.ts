a8f28469dd43c6a823b3eb1287ba0a8fe1b79bb4
Ben Hur <bhur777@gmail.com>
2023-08-24 13:12:10 -0400

    Create sales / stock count .ts

---
+++ actions/get-stock.count.ts
@@ -0,0 +1,10 @@
+export const getStockCount = async (storeId: string) => {
+	const stockCount = await prismadb.product.count({
+		where: {
+			storeId,
+			isArchived: false,
+		},
+	});
+
+	return stockCount;
+};
